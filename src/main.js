6e121c8bbb512d9a74ba6e46fc14e5baff56da31
NeXiDE <66691455+NeXiDE@users.noreply.github.com>
2021-02-15 14:11:05 +0100

    Added basic Shortcuts and added a command lines

--- src/main.js
+++ src/main.js
@@ -1,12 +1,22 @@
-const { app, BrowserWindow, globalShortcut } = require('electron');
+const { app, BrowserWindow, globalShortcut, clipboard } = require('electron');
 const path = require('path');
+const shortcut = require('electron-localshortcut');
 
 const isDev = require('electron-is-dev');
 
+app.commandLine.appendSwitch('high-dpi-support', 1);
+app.commandLine.appendSwitch('disable-frame-rate-limit');
+app.commandLine.appendSwitch('autoplay-policy', 'no-user-gesture-required');
+app.commandLine.appendSwitch('disable-accelerated-video-decode', false);
+app.commandLine.appendSwitch('disable-gpu-vsync');
+app.commandLine.appendSwitch('ignore-gpu-blacklist');
+app.commandLine.appendSwitch('enable-quic');
+app.commandLine.appendSwitch('enable-pointer-lock-options');
+
 function createWindow() {
   const win = new BrowserWindow({
-    width: 1280,
-    height: 720,
+    width: 1920,
+    height: 1080,
     webPreferences: {
       nodeIntegration: true,
       devTools: isDev,
@@ -16,12 +26,46 @@ function createWindow() {
 
   win.loadFile('src/public/index.html');
 
-  if (isDev) win.webContents.openDevTools();
+  // Crashes app.commandLine.appendSwitch function somehow. Better use Shortcut for opening it
+  //if (isDev) win.webContents.openDevTools();
+  if (!isDev) win.removeMenu();
+
+  shortcut.register(win, 'F1', () => {
+    win.loadFile('src/public/index.html');
+  });
+
+  shortcut.register(win, 'F3', () => {
+    var game = win.webContents.getURL().split('#').pop();
+    var url = 'https://venge.io/#' + game;
+    clipboard.writeText(url);
+    win.webContents
+      .executeJavaScript(
+        'pc.app.fire("Chat:Message", "[rainbow]Seven-Client[/rainbow]", "Link copied!")'
+      )
+      .catch((e) => {});
+  });
+
+  shortcut.register(win, 'Alt+F4', () => {
+    app.quit();
+  });
+
+  shortcut.register(win, 'F11', () => {
+    win.setSimpleFullScreen(!win.isSimpleFullScreen());
+  });
+
+  shortcut.register(win, 'F12', () => {
+    win.webContents.openDevTools();
+  });
 
   win.webContents.on('will-prevent-unload', (event) => event.preventDefault());
 
   globalShortcut.register('ESC', () => {
     win.webContents.executeJavaScript(`document.exitPointerLock()`);
+  });
+
+  win.webContents.on('dom-ready', (event) => {
+    iwin.setTitle(`Seven-Network-Client`);
+    event.preventDefault();
   });
 }
 
