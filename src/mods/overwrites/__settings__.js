38d8b83e70ca8745aaa3f3222243fd8e0a39b1ec
NeXiDE <66691455+NeXiDE@users.noreply.github.com>
2021-02-21 20:05:45 +0100

    Made Code prettier

--- src/mods/overwrites/__settings__.js
+++ src/mods/overwrites/__settings__.js
@@ -1,22 +1,28 @@
-ASSET_PREFIX = "";
-SCRIPT_PREFIX = "";
-SCENE_PATH = "905193.json?v=" + VERSION;
+ASSET_PREFIX = '';
+SCRIPT_PREFIX = '';
+SCENE_PATH = '905193.json?v=' + VERSION;
 CONTEXT_OPTIONS = {
-    'antialias': true,
-    'alpha': false,
-    'preserveDrawingBuffer': false,
-    'preferWebGl2': true,
-    'powerPreference': 'high-performance'
+  antialias: true,
+  alpha: false,
+  preserveDrawingBuffer: false,
+  preferWebGl2: true,
+  powerPreference: 'high-performance',
 };
 SCRIPTS = [];
-CONFIG_FILENAME = "config.json?v=" + VERSION;
+CONFIG_FILENAME = 'config.json?v=' + VERSION;
 INPUT_SETTINGS = {
-    useKeyboard: true,
-    useMouse: true,
-    useGamepads: false,
-    useTouch: true
+  useKeyboard: true,
+  useMouse: true,
+  useGamepads: false,
+  useTouch: true,
 };
 pc.script.legacy = false;
 PRELOAD_MODULES = [
-    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/31040882/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/31040883/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/31040881/1/ammo.js', 'preload' : true}
+  {
+    moduleName: 'Ammo',
+    glueUrl: 'files/assets/31040882/1/ammo.wasm.js',
+    wasmUrl: 'files/assets/31040883/1/ammo.wasm.wasm',
+    fallbackUrl: 'files/assets/31040881/1/ammo.js',
+    preload: true,
+  },
 ];
